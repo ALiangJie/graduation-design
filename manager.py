464717eccc2e0f7fc7e782617f778d84d4e46c27
jie liang <liangjie12058@outlook.com>
2022-03-30 19:21:28 +0800

    CSRFProtect保护

--- manager.py
+++ manager.py
@@ -11,6 +11,7 @@ from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_session import Session
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
@@ -51,17 +52,22 @@ redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode
 # 创建Session对象,读取App中Session配置信息
 Session(app)
 
+# 使用CSRFProtect保护app，验证机制，防止CSRF攻击，['POST', 'PUT', 'PATCH', 'DELETE']
+CSRFProtect(app)
+
+
+# 测试redis存取数据
+# redis_store.set("name", "laowang")
+# print(redis_store.get("name"))
+
+
+# 测试Session存取数据
+# session["name"] = "zhangsan"
+# print(session.get("name"))
 
 @app.route('/')
 def hello_world():
-    # 测试redis存取数据
-    redis_store.set("name", "laowang")
-    print(redis_store.get("name"))
     return "helloworld"
-
-    # 测试Session存取数据
-    session["name"] = "zhangsan"
-    print(session.get("name"))
 
 
 if __name__ == '__main__':
