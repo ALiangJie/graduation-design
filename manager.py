49b4d37507b88f5e9cb9225b9189292d60aca8a6
jie liang <liangjie12058@outlook.com>
2022-03-30 19:15:50 +0800

    session配置信息，测试session是否正常运行

--- manager.py
+++ manager.py
@@ -5,26 +5,39 @@
 3.session配置
 4.csrf配置
 """
+from datetime import timedelta
 
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_session import Session
 
 app = Flask(__name__)
 
 
 # 设置配置信息
 class Config(object):
-    # 调试信息
+    # 1.调试配置信息
     DEBUG = True
+    SECRET_KEY = "efsfsfsfef"
 
-    # 数据库配置信息
+    # 2.数据库配置信息
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@localhost:3306/infoVirus"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Redis配置信息
+    # 3.Redis配置信息
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+
+    # 4.Session配置信息
+    # 4.1设置Session存储类型
+    SESSION_TYPE = "redis"
+    # 4.2指定session存储的redis服务器
+    SESSION_PERMANENT = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 4.3设置签名存储
+    SESSION_USE_SIGNER = True
+    # 4.4设置session有效期
+    PERMANENT_SESSION_LIFETIME = timedelta(seconds=10)
 
 
 app.config.from_object(Config)
@@ -35,14 +48,20 @@ db = SQLAlchemy(app)
 # 创建Redis对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode_responses=True)
 
+# 创建Session对象,读取App中Session配置信息
+Session(app)
+
 
 @app.route('/')
 def hello_world():
-
     # 测试redis存取数据
-    redis_store.set("name","laowang")
+    redis_store.set("name", "laowang")
     print(redis_store.get("name"))
     return "helloworld"
+
+    # 测试Session存取数据
+    session["name"] = "zhangsan"
+    print(session.get("name"))
 
 
 if __name__ == '__main__':
