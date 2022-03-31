91093cfc07be0afa1740ac8ff97de1c421804eb7
jie liang <liangjie12058@outlook.com>
2022-03-31 11:22:36 +0800

    爬虫文件

---
+++ crawler/crawler_gene.py
@@ -0,0 +1,115 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+from selenium import webdriver
+import time
+import pymysql
+# 实现无可视化界面
+from selenium.webdriver.chrome.options import Options
+# 实现规避检测
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.keys import Keys
+
+def set_option():
+    """
+    谷歌浏览器常规反反爬的参数设置
+    """
+    # 实现无可视化界面的操作
+    chrome_options = Options()
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    # 实现规避检测
+    option = ChromeOptions()
+    option.add_experimental_option("excludeSwitches",["enable-automation"])
+    return chrome_options, option
+
+
+if __name__ == '__main__':
+    chrome_options, option = set_option()
+    executable_path = "../chromedriver_win32/chromedriver.exe"
+
+    # 连接database
+    conn = pymysql.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="123456",
+        database="info36",
+        charset="utf8"
+    )
+
+    # 得到一个可以执行SQL语句的光标对象
+    cursor = conn.cursor()
+    create_time = ''
+    update_time = ''
+    clicks = '0'
+    category_id = '2'
+    success_count = 1
+    index_image_url = ''
+    user_id = None
+    status = '0'
+    reason = ''
+    # 让selenium规避被检测到的风险
+    bro = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options, options=option)
+    # 爬取的url主页
+    bro.get(url='https://www.ncbi.nlm.nih.gov/gene/?term=virus')
+    # 获取当前页源码数据
+    page_text = bro.page_source
+    index_page_tree = etree.HTML(page_text)
+
+    all_page = int(index_page_tree.xpath('//*[@id="pageno2"]/@last')[0])
+
+    init_url = "https://www.ncbi.nlm.nih.gov"
+
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+    }
+
+    start_page = 1
+    for page_num in range(start_page, 6):
+        # 模拟浏览器换页
+        bro.find_element_by_id('pageno').click()
+        bro.find_element_by_id('pageno').clear()
+        bro.find_element_by_id('pageno').send_keys(page_num)
+        bro.find_element_by_id('pageno').send_keys(Keys.ENTER)
+        print("正在爬取第" + str(page_num) + "页。。。")
+        # 获取当前页的源码页面
+        cru_page = bro.page_source
+        cru_page_text = etree.HTML(cru_page)
+        tr_list = cru_page_text.xpath('//*[@id="ui-ncbigrid-7"]/tbody/tr')
+        # 解析数据
+        for tr in tr_list:
+            virus_gene_name = tr.xpath('./td[1]/div[2]/a//text()')[0]
+            virus_gene_id = str(tr.xpath('./td[1]/span//text()')[0])
+            virus_gene_description = ''.join(tr.xpath('./td[2]//text()'))
+
+            # 拼接url加上id
+            detail_url = init_url + tr.xpath('.//a/@href')[0]
+            response = requests.get(detail_url, headers=headers)  # 得到响应
+            detail_page_text = response.text
+            soup = BeautifulSoup(detail_page_text, 'lxml')
+            title_div = soup.findAll(name="div", attrs={"class": re.compile("rprt-header")})
+            summary_div = soup.findAll(name="div", attrs={"class": re.compile("rprt-section gene-summary")})
+            content = str(title_div[0]) + str(summary_div[0])
+
+            create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            update_time = create_time
+            # SQL 插入语句
+            sql = "insert into info_news (create_time, update_time, title, source, digest, content, clicks, index_image_url, category_id, user_id, status, reason) " \
+                  "values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            # 执行SQL语句
+            cursor.execute(sql,
+                           [create_time, update_time, virus_gene_name, virus_gene_id, virus_gene_description, content,
+                            clicks, index_image_url, category_id, user_id, status, reason])
+            # 提交事务
+            conn.commit()
+            print("插入第", success_count, "组数据成功！")
+            success_count += 1
+            # print("名称：" + virus_gene_name, virus_gene_id, "简述：" + virus_gene_description + content)
+        print("爬取第" + str(page_num) + "页完成！")
+        time.sleep(5)
+    # 关闭数据库连接
+    cursor.close()
+    conn.close()
---
+++ crawler/crawler_nucleotide.py
@@ -0,0 +1,119 @@
+import requests
+from lxml import etree
+from selenium import webdriver
+import time
+import pymysql
+# 实现无可视化界面
+from selenium.webdriver.chrome.options import Options
+# 实现规避检测
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.keys import Keys
+
+def set_option():
+    """
+    谷歌浏览器常规反反爬的参数设置
+    """
+    # 实现无可视化界面的操作
+    chrome_options = Options()
+    chrome_options.add_experimental_option(
+        'excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    # 实现规避检测
+    option = ChromeOptions()
+    option.add_experimental_option("excludeSwitches",
+                                   ["enable-automation"])
+    return chrome_options, option
+
+
+if __name__ == '__main__':
+    chrome_options, option = set_option()
+    executable_path = "../chromedriver_win32/chromedriver.exe"
+
+    # 连接database
+    conn = pymysql.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="123456",
+        database="info36",
+        charset="utf8"
+    )
+
+    # 得到一个可以执行SQL语句的光标对象
+    cursor = conn.cursor()
+    create_time = ''
+    update_time = ''
+    clicks = '0'
+    category_id = '3'
+    success_count = 1
+    index_image_url = ''
+    user_id = None
+    status = '0'
+    reason = ''
+
+    # 让selenium规避被检测到的风险
+    bro = webdriver.Chrome(executable_path=executable_path, chrome_options=chrome_options, options=option)
+    # 爬取的url主页
+    bro.get(url='https://www.ncbi.nlm.nih.gov/nuccore/?term=virus')
+    # 获取当前页源码数据
+    page_text = bro.page_source
+    index_page_tree = etree.HTML(page_text)
+
+    all_page = int(index_page_tree.xpath('//*[@id="pageno2"]/@last')[0])
+
+    init_url_first = "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id="
+    init_url_end = "&db=nuccore&report=genbank&conwithfeat=on&hide-cdd=on&retmode=html&withmarkup=on&tool=portal&log$=seqview&maxdownloadsize=1000000"
+
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+    }
+
+    start_page = 6
+    for page_num in range(start_page, 10):
+        # 模拟浏览器换页
+        # 模拟浏览器换页
+        bro.find_element_by_id('pageno').click()
+        bro.find_element_by_id('pageno').clear()
+        bro.find_element_by_id('pageno').send_keys(page_num)
+        bro.find_element_by_id('pageno').send_keys(Keys.ENTER)
+        print("正在爬取第" + str(page_num) + "页。。。")
+
+        # 获取当前页的源码页面
+        cru_page = bro.page_source
+        cru_page_text = etree.HTML(cru_page)
+        tr_list = cru_page_text.xpath('//*[@id="maincontent"]/div/div[5]/div')
+        # 解析数据
+        for tr in tr_list:
+            virus_nucleotide_name = tr.xpath('./div[2]/p/a//text()')[0]
+            virus_nucleotide_id = tr.xpath('./div[2]/div[2]/div/dl/dd[2]/text()')[0]
+            virus_nucleotide_description = tr.xpath('./div[2]/div[1]/p/text()')[0]
+
+            detail_url = init_url_first + virus_nucleotide_id + init_url_end
+            response = requests.get(detail_url, headers=headers)  # 得到响应
+            detail_page_text = response.text
+            content = detail_page_text
+
+            create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            update_time = create_time
+
+            # SQL 插入语句
+            sql = "insert into info_news (create_time, update_time, title, source, digest, content, clicks, index_image_url, category_id, user_id, status, reason) " \
+                  "values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            # 执行SQL语句
+            cursor.execute(sql,
+                           [create_time, update_time, virus_nucleotide_name, virus_nucleotide_id,
+                            virus_nucleotide_description, content,
+                            clicks, index_image_url, category_id, user_id, status, reason])
+            # 提交事务
+            conn.commit()
+            print("插入第", success_count, "组数据成功！")
+            print(virus_nucleotide_name,virus_nucleotide_id,virus_nucleotide_description,content)
+            input()
+            success_count += 1
+        print("爬取第" + str(page_num) + "页完成！")
+
+
+    # 关闭数据库连接
+    cursor.close()
+    conn.close()
