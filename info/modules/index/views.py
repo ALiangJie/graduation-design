e6ed2969cff5a4aacbee1357d83a1c5b028bd91b
jie liang <liangjie12058@outlook.com>
2022-04-02 16:21:44 +0800

    用户名称显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -3,15 +3,9 @@ from info.modules.index import index_blue
 from flask import render_template, current_app, session
 
 
-# 用户登录显示
-@index_blue.route('/', methods=["POST", "GET"])
-def user_login_show():
-    return render_template("user_login.html")
-
-
-# 主界面的用户显示
-@index_blue.route('/index', methods=["POST", "GET"])
-def index_show():
+# 主页的用户显示
+@index_blue.route('/user_index', methods=["POST", "GET"])
+def user_index_show():
     # 1.获取用户的登录信息
     user_id = session.get("user_id")
 
@@ -28,11 +22,28 @@ def index_show():
         # 如果user有值返回左边的内容，否则返回右边的值
         "user_info": user.to_dict() if user else ""
     }
+    return render_template("user_index.html", data=data)
 
-    return render_template("user/index.html", data=data)
+
+# 用户登录界面显示
+@index_blue.route('/', methods=["POST", "GET"])
+def user_login_show():
+    return render_template("user_login.html")
+
+
+# 欢迎页面显示
+@index_blue.route('/user_welcome', methods=["POST", "GET"])
+def user_welcome_index_show():
+    return render_template("user_welcome.html")
+
+
+# 处理网站favicon.ico
+@index_blue.route('/favicon.ico')
+def get_web_favicon():
+    return current_app.send_static_file('images/favicon.ico')
 
 
 # 处理网站logo
-@index_blue.route('/favicon.ico')
+@index_blue.route('/images/logo.png')
 def get_web_logo():
-    return current_app.send_static_file('images/favicon.ico')
+    return current_app.send_static_file('images/logo.png')
--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -13,6 +13,26 @@ from info.response_code import RET
 from info.utils.captcha.captcha import captcha
 
 
+# 退出登陆
+# 请求路径: /passport/logout
+# 请求方式: POST
+# 请求参数: 无
+# 返回值: errno, errmsg
+@passport_blue.route('/logout', methods=['POST'])
+def logout():
+    """
+    1.清除session信息
+    2.返回响应
+    :return:
+    """
+    # 1.清除session信息
+    session.pop("user_id", None)
+    session.pop("is_admin", None)
+
+    # 2.返回响应
+    return jsonify(errno=RET.OK, errmsg="退出成功")
+
+
 # 登陆用户
 # 请求路径: /passport/login
 # 请求方式: POST
